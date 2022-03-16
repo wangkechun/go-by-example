af500ae99f590f42d1bb64341dc31ee4dec88b85
wangkechun <wangkechun@bytedance.com>
2022-03-16 16:02:23 +0800

    v1

--- simpledict/v1/main.go
+++ simpledict/v1/main.go
@@ -1,110 +1,46 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
+	"strings"
 )
 
-type DictRequest struct {
-	TransType string `json:"trans_type"`
-	Source    string `json:"source"`
-	UserID    string `json:"user_id"`
-}
-
-type DictResponse struct {
-	Rc   int `json:"rc"`
-	Wiki struct {
-		KnownInLaguages int `json:"known_in_laguages"`
-		Description     struct {
-			Source string      `json:"source"`
-			Target interface{} `json:"target"`
-		} `json:"description"`
-		ID   string `json:"id"`
-		Item struct {
-			Source string `json:"source"`
-			Target string `json:"target"`
-		} `json:"item"`
-		ImageURL  string `json:"image_url"`
-		IsSubject string `json:"is_subject"`
-		Sitelink  string `json:"sitelink"`
-	} `json:"wiki"`
-	Dictionary struct {
-		Prons struct {
-			EnUs string `json:"en-us"`
-			En   string `json:"en"`
-		} `json:"prons"`
-		Explanations []string      `json:"explanations"`
-		Synonym      []string      `json:"synonym"`
-		Antonym      []string      `json:"antonym"`
-		WqxExample   [][]string    `json:"wqx_example"`
-		Entry        string        `json:"entry"`
-		Type         string        `json:"type"`
-		Related      []interface{} `json:"related"`
-		Source       string        `json:"source"`
-	} `json:"dictionary"`
-}
-
-func query(word string) error {
-
-	url := "https://api.interpreter.caiyunai.com/v1/dict"
-
-	request := DictRequest{TransType: "en2zh", Source: word}
-	buf, err := json.Marshal(request)
-	if err != nil {
-		return fmt.Errorf("json.Marshal %w", err)
-	}
-	payload := bytes.NewReader(buf)
-
-	req, err := http.NewRequest("POST", url, payload)
-	if err != nil {
-		return fmt.Errorf("http.NewRequest %w", err)
-	}
-
-	req.Header.Add("user-agent", "vscode-restclient")
-	req.Header.Add("content-type", "application/json;charset=UTF-8")
-	req.Header.Add("x-authorization", "token:qgemv4jr1y38jyq6vhvi")
-
-	res, _ := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("http.DefaultClient.Do %w", err)
-	}
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("ioutil.ReadAll %w", err)
-	}
-	if res.StatusCode != 200 {
-		return fmt.Errorf("bad body: %s", body)
-	}
-	// fmt.Println(res)
-	// fmt.Println(string(body))
-	var resp DictResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		return fmt.Errorf("json.Unmarshal %w", err)
-	}
-	// fmt.Println(resp)
-	fmt.Println(word, "UK:", resp.Dictionary.Prons.En, "US:", resp.Dictionary.Prons.EnUs)
-	for _, item := range resp.Dictionary.Explanations {
-		fmt.Println(item)
-	}
-	return nil
-}
-
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, `usage: simpleDict WORD
-example: simpleDict hello
-		`)
-		os.Exit(1)
-	}
-	word := os.Args[1]
-	err := query(word)
+	client := &http.Client{}
+	var data = strings.NewReader(`{"trans_type":"en2zh","source":"good"}`)
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
 	if err != nil {
-		fmt.Printf("query error: %+v\n", err)
+		log.Fatal(err)
 	}
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("DNT", "1")
+	req.Header.Set("os-version", "")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("device-id", "")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("X-Authorization", "token:qgemv4jr1y38jyq6vhvi")
+	req.Header.Set("Origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("Sec-Fetch-Site", "cross-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cookie", "_ym_uid=16456948721020430059; _ym_d=1645694872")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
