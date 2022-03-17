39da23b7e56d4174b31e194f7c8b324310743b66
wangkechun <wangkechun@bytedance.com>
2022-03-17 14:17:29 +0800

    better

--- proxy/main.go
+++ proxy/main.go
@@ -11,8 +11,11 @@ import (
 	"net"
 )
 
-const SOCKS5_VER = 0x05
-const CMD_BIND = 0x01
+const socks5Ver = 0x05
+const cmdBind = 0x01
+const atypIPV4 = 0x01
+const atypeHOST = 0x03
+const atypeIPV6 = 0x04
 
 func main() {
 	server, err := net.Listen("tcp", "127.0.0.1:1080")
@@ -45,11 +48,22 @@ func process(conn net.Conn) {
 }
 
 func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+----------+----------+
+	// |VER | NMETHODS | METHODS  |
+	// +----+----------+----------+
+	// | 1  |    1     | 1 to 255 |
+	// +----+----------+----------+
+	// VER: 协议版本，socks5为0x05
+	// NMETHODS: 支持认证的方法数量
+	// METHODS: 对应NMETHODS，NMETHODS的值为多少，METHODS就有多少个字节。RFC预定义了一些值的含义，内容如下:
+	// X’00’ NO AUTHENTICATION REQUIRED
+	// X’02’ USERNAME/PASSWORD
+
 	ver, err := reader.ReadByte()
 	if err != nil {
 		return fmt.Errorf("read ver failed:%w", err)
 	}
-	if ver != SOCKS5_VER {
+	if ver != socks5Ver {
 		return fmt.Errorf("not supported ver:%v", ver)
 	}
 	methodSize, err := reader.ReadByte()
@@ -61,39 +75,55 @@ func auth(reader *bufio.Reader, conn net.Conn) (err error) {
 	if err != nil {
 		return fmt.Errorf("read method failed:%w", err)
 	}
-	_, err = conn.Write([]byte{SOCKS5_VER, 0x00})
+
+	// +----+--------+
+	// |VER | METHOD |
+	// +----+--------+
+	// | 1  |   1    |
+	// +----+--------+
+	_, err = conn.Write([]byte{socks5Ver, 0x00})
 	if err != nil {
 		return fmt.Errorf("write failed:%w", err)
 	}
 	return nil
 }
 
-const ATYP_IPV4 = 0x01
-const ATYP_HOST = 0x03
-const ATYP_IPV6 = 0x04
-
 func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	// +----+-----+-------+------+----------+----------+
+	// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+	// VER 版本号，socks5的值为0x05
+	// CMD 0x01表示CONNECT请求
+	// RSV 保留字段，值为0x00
+	// ATYP 目标地址类型，DST.ADDR的数据对应这个字段的类型。
+	//   0x01表示IPv4地址，DST.ADDR为4个字节
+	//   0x03表示域名，DST.ADDR是一个可变长度的域名
+	// DST.ADDR 一个可变长度的值
+	// DST.PORT 目标端口，固定2个字节
+
 	buf := make([]byte, 4)
 	_, err = io.ReadFull(reader, buf)
 	if err != nil {
 		return fmt.Errorf("read header failed:%w", err)
 	}
 	ver, cmd, atyp := buf[0], buf[1], buf[3]
-	if ver != SOCKS5_VER {
+	if ver != socks5Ver {
 		return fmt.Errorf("not supported ver:%v", ver)
 	}
-	if cmd != CMD_BIND {
+	if cmd != cmdBind {
 		return fmt.Errorf("not supported cmd:%v", ver)
 	}
 	addr := ""
 	switch atyp {
-	case ATYP_IPV4:
+	case atypIPV4:
 		_, err = io.ReadFull(reader, buf)
 		if err != nil {
 			return fmt.Errorf("read atyp failed:%w", err)
 		}
 		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
-	case ATYP_HOST:
+	case atypeHOST:
 		hostSize, err := reader.ReadByte()
 		if err != nil {
 			return fmt.Errorf("read hostSize failed:%w", err)
@@ -104,7 +134,7 @@ func connect(reader *bufio.Reader, conn net.Conn) (err error) {
 			return fmt.Errorf("read host failed:%w", err)
 		}
 		addr = string(host)
-	case ATYP_IPV6:
+	case atypeIPV6:
 		return errors.New("IPv6: no supported yet")
 	default:
 		return errors.New("invalid atyp")
@@ -121,12 +151,18 @@ func connect(reader *bufio.Reader, conn net.Conn) (err error) {
 	}
 	defer dest.Close()
 	log.Println("dial", addr, port)
+
 	// +----+-----+-------+------+----------+----------+
 	// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
 	// +----+-----+-------+------+----------+----------+
 	// | 1  |  1  | X'00' |  1   | Variable |    2     |
 	// +----+-----+-------+------+----------+----------+
-
+	// VER socks版本，这里为0x05
+	// REP Relay field,内容取值如下 X’00’ succeeded
+	// RSV 保留字段
+	// ATYPE 同请求的ATYPE
+	// BND.ADDR 服务绑定的地址
+	// BND.PORT 服务绑定的端口DST.PORT
 	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
 	if err != nil {
 		return fmt.Errorf("write failed: %w", err)
