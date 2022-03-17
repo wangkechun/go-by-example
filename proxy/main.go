b90a8a4c6225b872e5b93d2b4546170300812900
wangkechun <wangkechun@bytedance.com>
2022-03-17 11:57:22 +0800

    add proxy

---
+++ proxy/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const SOCKS5_VER = 0x05
+const CMD_BIND = 0x01
+
+func main() {
+	server, err := net.Listen("tcp", "127.0.0.1:1080")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		client, err := server.Accept()
+		if err != nil {
+			log.Printf("Accept failed %v", err)
+			continue
+		}
+		go process(client)
+	}
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	err := auth(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+	err = connect(reader, conn)
+	if err != nil {
+		log.Printf("client %v auth failed:%v", conn.RemoteAddr(), err)
+		return
+	}
+}
+
+func auth(reader *bufio.Reader, conn net.Conn) (err error) {
+	ver, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read ver failed:%w", err)
+	}
+	if ver != SOCKS5_VER {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	methodSize, err := reader.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read methodSize failed:%w", err)
+	}
+	method := make([]byte, methodSize)
+	_, err = io.ReadFull(reader, method)
+	if err != nil {
+		return fmt.Errorf("read method failed:%w", err)
+	}
+	_, err = conn.Write([]byte{SOCKS5_VER, 0x00})
+	if err != nil {
+		return fmt.Errorf("write failed:%w", err)
+	}
+	return nil
+}
+
+const ATYP_IPV4 = 0x01
+const ATYP_HOST = 0x03
+const ATYP_IPV6 = 0x04
+
+func connect(reader *bufio.Reader, conn net.Conn) (err error) {
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return fmt.Errorf("read header failed:%w", err)
+	}
+	ver, cmd, atyp := buf[0], buf[1], buf[3]
+	if ver != SOCKS5_VER {
+		return fmt.Errorf("not supported ver:%v", ver)
+	}
+	if cmd != CMD_BIND {
+		return fmt.Errorf("not supported cmd:%v", ver)
+	}
+	addr := ""
+	switch atyp {
+	case ATYP_IPV4:
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return fmt.Errorf("read atyp failed:%w", err)
+		}
+		addr = fmt.Sprintf("%d.%d.%d.%d", buf[0], buf[1], buf[2], buf[3])
+	case ATYP_HOST:
+		hostSize, err := reader.ReadByte()
+		if err != nil {
+			return fmt.Errorf("read hostSize failed:%w", err)
+		}
+		host := make([]byte, hostSize)
+		_, err = io.ReadFull(reader, host)
+		if err != nil {
+			return fmt.Errorf("read host failed:%w", err)
+		}
+		addr = string(host)
+	case ATYP_IPV6:
+		return errors.New("IPv6: no supported yet")
+	default:
+		return errors.New("invalid atyp")
+	}
+	_, err = io.ReadFull(reader, buf[:2])
+	if err != nil {
+		return fmt.Errorf("read port failed:%w", err)
+	}
+	port := binary.BigEndian.Uint16(buf[:2])
+
+	dest, err := net.Dial("tcp", fmt.Sprintf("%v:%v", addr, port))
+	if err != nil {
+		return fmt.Errorf("dial dst failed:%w", err)
+	}
+	defer dest.Close()
+	log.Println("dial", addr, port)
+	// +----+-----+-------+------+----------+----------+
+	// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	// +----+-----+-------+------+----------+----------+
+	// | 1  |  1  | X'00' |  1   | Variable |    2     |
+	// +----+-----+-------+------+----------+----------+
+
+	_, err = conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0})
+	if err != nil {
+		return fmt.Errorf("write failed: %w", err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		_, _ = io.Copy(dest, reader)
+		cancel()
+	}()
+	go func() {
+		_, _ = io.Copy(conn, dest)
+		cancel()
+	}()
+
+	<-ctx.Done()
+	return nil
+}
