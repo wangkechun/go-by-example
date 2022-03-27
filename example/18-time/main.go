ff8ce65a9ae64d77f17d23cf1288a518a4f230da
wangkechun <wangkechun@bytedance.com>
2022-03-27 18:22:27 +0800

    add example

---
+++ example/18-time/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println(now) // 2022-03-27 18:04:59.433297 +0800 CST m=+0.000087933
+	t := time.Date(2022, 3, 27, 1, 25, 36, 0, time.UTC)
+	t2 := time.Date(2022, 3, 27, 2, 30, 36, 0, time.UTC)
+	fmt.Println(t)                                                  // 2022-03-27 01:25:36 +0000 UTC
+	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute()) // 2022 March 27 1 25
+	fmt.Println(t.Format("2006-01-02 15:04:05"))                    // 2022-03-27 01:25:36
+	diff := t2.Sub(t)
+	fmt.Println(diff)                           // 1h5m0s
+	fmt.Println(diff.Minutes(), diff.Seconds()) // 65 3900
+	t3, err := time.Parse("2006-01-02 15:04:05", "2022-03-27 01:25:36")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(t3 == t) // true
+}
---
+++ example/19-strconv/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	f, _ := strconv.ParseFloat("1.234", 64)
+	fmt.Println(f) // 1.234
+
+	n, _ := strconv.ParseInt("111", 10, 64)
+	fmt.Println(n) // 111
+
+	n, _ = strconv.ParseInt("0x1000", 0, 64)
+	fmt.Println(n) // 4096
+
+	n2, _ := strconv.Atoi("123")
+	fmt.Println(n2) // 123
+
+	n2, err := strconv.Atoi("AAA")
+	fmt.Println(n2, err) // 0 strconv.Atoi: parsing "AAA": invalid syntax
+}
