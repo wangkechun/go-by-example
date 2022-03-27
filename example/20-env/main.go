e2cb16d57870527927d2006734d925a22c0dc1e5
wangkechun <wangkechun@bytedance.com>
2022-03-27 18:22:27 +0800

    add example

---
+++ example/20-env/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// go run example/20-env/main.go a b c d
+	fmt.Println(os.Args)           // [/var/folders/8p/n34xxfnx38dg8bv_x8l62t_m0000gn/T/go-build3406981276/b001/exe/main a b c d]
+	fmt.Println(os.Getenv("PATH")) // /usr/local/go/bin...
+	fmt.Println(os.Setenv("AA", "BB"))
+
+	buf, err := exec.Command("grep", "127.0.0.1", "/etc/hosts").CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(buf)) // 127.0.0.1       localhost
+}
