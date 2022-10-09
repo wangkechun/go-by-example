60f7d68c7e9aa098a9beb45e6a5cc8c80ee20b28
wangkechun <wangkechun@bytedance.com>
2022-10-09 15:31:59 +0800

    fix read line

--- guessing-game/v3/main.go
+++ guessing-game/v3/main.go
@@ -23,7 +23,7 @@ func main() {
 		fmt.Println("An error occured while reading input. Please try again", err)
 		return
 	}
-	input = strings.TrimSuffix(input, "\n")
+	input = strings.Trim(input, "\r\n")
 
 	guess, err := strconv.Atoi(input)
 	if err != nil {
