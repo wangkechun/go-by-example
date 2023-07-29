997d1d2715a7db9c10de2249fc8f9a7a76ac3f33
郭睆 <gh2002@mail.nwpu.edu.en>
2023-07-29 20:26:00 +0800

    add：guessing-game\v1，添加注释，补充从1.10开始rand包自动初始化

--- guessing-game/v1/main.go
+++ guessing-game/v1/main.go
@@ -10,4 +10,6 @@ func main() {
 	secretNumber := rand.Intn(maxNum)
 	fmt.Println("The secret number is ", secretNumber)
 	// 新版本（我的为1.20.6）已经可以每次输出不同的数了
+	// 查阅资料得，从1.10版本开始
+	// math/rand 包会自动初始化一个全局公共源，并默认使用当前时间作为种子，因此不再需要手动设置种子值。
 }
