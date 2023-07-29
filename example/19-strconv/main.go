557e9a98bd2adf5412d3b2474dd1beb2886055de
郭睆 <gh2002@mail.nwpu.edu.en>
2023-07-29 20:00:53 +0800

    add：19-strconv，添加PaserInt基数为零的解释，将Atoi函数的官方注释抄入之后

--- example/19-strconv/main.go
+++ example/19-strconv/main.go
@@ -12,10 +12,10 @@ func main() {
 	n, _ := strconv.ParseInt("111", 10, 64)
 	fmt.Println(n) // 111
 
-	n, _ = strconv.ParseInt("0x1000", 0, 64)
+	n, _ = strconv.ParseInt("0x1000", 0, 64) // 基数为零表示自动识别进制，这里通过0x前缀来判断为十六进制
 	fmt.Println(n) // 4096
 
-	n2, _ := strconv.Atoi("123")
+	n2, _ := strconv.Atoi("123") // Atoi is equivalent to ParseInt(s, 10, 0), converted to type int.
 	fmt.Println(n2) // 123
 
 	n2, err := strconv.Atoi("AAA")
